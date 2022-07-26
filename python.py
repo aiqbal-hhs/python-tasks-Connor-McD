857d403c4d6fac4d6f68990032d417622379c671
Connor <79290055+Connor-McD@users.noreply.github.com>
2022-07-27 09:56:31 +1200

    Add files via upload

--- python.py
+++ python.py
@@ -10,18 +10,28 @@ total_price = []
 finished_order = {}
 customer_address = {}
 
+#To show the menu to user
+def ShowMenu():
+    os.system('cls')
+    print("Pizzas Available:\n")
+    print(*pizzas_available,sep = ', ')
+    print("\n\nToppings Available:\n")
+    print(*toppings_available,sep = ', ')
+    print('\n\n')
 
-
+#asking customer for pizza order
 def greet(name):
     """
-    This function greets 
-    the user 
+    This function greets to
+    the person passed in as
+    a parameter
     """
     print("Hello " + name + ", Welcome to Henderson Pizza Store!")
 
 greet('User')
 
-#asking customer for pizza order
+
+
 order_pizza = True
 while order_pizza:    
     print("could you please choose a pizza you would like to order: ")
@@ -38,6 +48,7 @@ while order_pizza:
         if pizza not in pizzas_available:
             print(f"I'm sorry but unfortunately we do not have {pizza} in our menu.")
 
+
     #asking for extra toppings
     order_topping = True
     print("This is the types of extra toppings that are available:: ")
@@ -45,8 +56,8 @@ while order_pizza:
         print(toppings)
         print()
     while order_topping:
-        other_topping = input("Would you like any other toppings? yes or no?")
-        if other_topping == "yes" or other_topping == "Yes" or other_topping == "y" or other_topping == "Y":
+        other_topping = input("Would you like an other toppings? yes or no?")
+        if other_topping == "yes":
            
             topping = input("Which one would you like to add on to your pizza?")
             if topping in toppings_available:
@@ -57,23 +68,20 @@ while order_pizza:
             else:
                 print(f"I am sorry, we don't have {topping} available on our menu.")
 
-        elif other_topping == "no" or other_topping == "No" or other_topping == "N" or other_topping == "n":
+        elif other_topping == "no":
             break
-
-    other_pizza = input("Would you like to order any other pizzas? yes or no")
-
-    if other_pizza == "no" or other_pizza == "No" or other_pizza == "n" or other_pizza == "N":
+    other_pizza = input("Would you like to order any other pizzas?")
+    if other_pizza == "no":
         for key, value in finished_order.items():
             print(f"\nYou have ordered one {key} pizza with {value}")
         check_order = True
         while check_order:
             print()
-
             order_correct = input("Is this correct? yes/no ")
-            if other_pizza == "no" or  other_pizza == "No" or other_pizza == "n" or  other_pizza == "N" :
+            if order_correct == "yes":
                 check_order = False
                 order_pizza = False
-            if order_correct == "no" or order_correct == "No" or order_correct == "N" or order_correct == "n":
+            if order_correct == "no":
                 print(finished_order)
                 add_remove = input("would you like to add or remove? ")
                 if add_remove == "remove":
@@ -88,7 +96,7 @@ print(f"\nYour total order price is: ${sum(total_price)}")
 
 print("Please provide us with your name, address and phonenumber")
 customer_address['name'] = input("what is your name?")
-customer_address['street_name'] = input("What is your housenumber and streetname?")
+customer_address['street_name'] = input("What is your housenumber and streetname  ?")
 customer_address['postalcode'] = input("What is the postalcode?")
 customer_address['phonenumber'] = input("What is your phonenumber?")
 print()
