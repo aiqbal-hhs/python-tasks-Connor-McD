0fbf3d87b2b65e9c99c73d214bd5a498d396d502
Connor <79290055+Connor-McD@users.noreply.github.com>
2022-09-28 09:45:48 +1300

    Add files via upload

--- update.py
+++ update.py
@@ -6,14 +6,14 @@ toppings_listed = ['Jalapeno', 'Onions', 'Capsicum', 'Extra Cheese', 'Olives', '
 pizza_prices = {'Margarita': 8.50, 'Pepperoni': 8.50, 'Meatlovers': 8.50, 'Butter Chicken': 8.50, 'Beef And Onion': 8.50, 'Hawaiian': 8.50, 'Cheese Supreme': 8.50, 'Cheese And Garlic': 5, 'Vegetarian': 5, 'Chicken Cranberry': 5, 'Buffalo Chicken': 5, 'Ham And Cheese': 5, 'Peri Peri Chicken': 5}
 topping_prices = {'Jalapeno':0.50, 'Onions': 0.50, 'Capsicum':0.50, 'Extra Cheese':0.50, 'Olives':0.50, 'Tomatoes':0.50, 'Mushrooms': 0.50, 'Spinach': 0.50, 'Extra Pepperoni': 0.50, 'Bacon': 0.50, 'Sausage': 0.50, 'Pineapple': 0.50, 'Wedges': 0.50}
 finished_price = []
-finished_order = {}
+order_finished = {}
 customer_information = {}
 
 
-#asking customer for pizza order
+#asking the user to select a pizza from the menu
 print("Welcome to Henderson Pizza Store, please select a pizza that you would like to order ")
-order_pizza = True
-while order_pizza:    
+pizza_ordered = True
+while pizza_ordered:    
     print("Could you please choose a pizza that you would like to order from the menu provided: ")
     print()
     for pizzas in pizzas_listed:
@@ -29,7 +29,7 @@ while order_pizza:
         if pizza not in pizzas_listed:
             print(f"I'm sorry but unfortunately we do not have {pizza} in our menu please try again. ")
 
-    #asking for extra toppings
+    #asking the user to choose extra toppings for their pizza
     order_topping = True
     print("These are the types of extra toppings that are available to put on your pizza:: ")
     for toppings in toppings_listed:
@@ -40,8 +40,8 @@ while order_pizza:
         if other_topping == "yes" or other_topping == "Yes" or other_topping == "y" or other_topping == "Y":
             topping = input("Which one would you like to add onto your pizza? ").title()
             if topping in toppings_listed:
-                finished_order.setdefault(pizza, [])
-                finished_order[pizza].append(topping)
+                order_finished.setdefault(pizza, [])
+                order_finished[pizza].append(topping)
                 print(f"I have added {topping} onto your pizza. ")
                 finished_price.append(topping_prices[topping])
             else:
@@ -51,7 +51,7 @@ while order_pizza:
             break
     other_pizza = input("Would you like to order any other pizzas? ")
     if other_pizza == "no" or other_pizza == "No" or other_pizza == "n" or other_pizza == "N" :
-        for key, value in finished_order.items():
+        for key, value in order_finished.items():
             print(f"\nYou have ordered one {key} pizza with {value} ")
         check_order = True
         while check_order:
@@ -59,18 +59,18 @@ while order_pizza:
             order_correct = input("Is this correct? yes/no ")
             if order_correct == "yes" or order_correct == "Yes" or order_correct == "y" or order_correct == "Y":
                 check_order = False
-                order_pizza = False
+                pizza_ordered = False
             if order_correct == "no" or order_correct == "No" or order_correct == "n" or order_correct == "N":
-                print(finished_order)
+                print(order_finished)
                 add_remove = input("would you like to add or remove anything you have chosen? ")
                 if add_remove == "remove":
                     remove = input("Which pizza would you like to remove? ")
-                    del finished_order[remove]
-                    print(finished_order)
+                    del order_finished[remove]
+                    print(order_finished)
                 if add_remove == "add":
                     check_order = False
 
-#finalizing the order
+#finishing off the customers order
 print(f"\nYour total order price is: ${sum(finished_price)} + $3 delivery fee")
 
 print("Please provide us with your name, address, postalcode and phonenumber" )
